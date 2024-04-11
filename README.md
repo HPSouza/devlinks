91aa5712b370fed927f7a123f0ff48706bcc82aa
Henrique Souza <henriquesouza2697@gmail.com>
2024-04-10 23:25:26 -0300

    final-commit

---
+++ README.md
@@ -0,0 +1,34 @@
+<h1 align="center"> DevLinks </h1>
+
+<p align="center">
+Esse código foi desenvolvido a partir do Discover que é um programa exclusivo e gratuito, promovido pela Rocketseat para ensino de tecnologias WEB. <br/>
+</p>
+
+<p align="center">
+  <a href="#-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-projeto">Projeto</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-projeto">Aprendizado</a>
+</p>
+
+<br>
+
+<p align="center">
+  <img alt="projeto DevLinks" src=".github/preview.png" width="100%">
+</p>
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido utilizado as tecnologias:
+
+- HTML e CSS
+- JavaScript
+- Git e Github
+- Figma
+
+## 💻 Projeto
+
+O DevLinks é um agregador de links para usar como cartão de visitas online.
+
+## 📖 Aprendizado
+
+Com o DevLinks consegui concretizar conceitos básico de html e css. Além disso implementei de forma extra a funcionalidade de tradução das páginas utilizando eventos do Javascript.
--- script.js
+++ script.js
@@ -1,14 +1,56 @@
+var defaultTitle = "DevLinks - Henrique Souza"
+
 function toogleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
 
   const img = document.querySelector("#profile img")
+  const isLightMode = html.classList.contains("light")
 
-  if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar-light.png")
+  if (isLightMode) {
+    img.setAttribute("src", "./assets/avatar.png")
     img.setAttribute("alt", "Profile image light")
   } else {
     img.setAttribute("src", "./assets/avatar.png")
     img.setAttribute("alt", "Profile image")
   }
+
+  localStorage.setItem("isLightMode", isLightMode)
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  var deviceLanguage = getDeviceLanguage()
+  if (deviceLanguage.includes("en-")) loadTranslations("en-US")
+
+  if (localStorage.getItem("isLightMode") === "true") {
+    const html = document.documentElement
+    html.classList.add("light")
+  }
+})
+
+function getDeviceLanguage() {
+  return navigator.language
+}
+
+function loadTranslations(language) {
+  fetch("translations.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const translations = data[language]
+      const aboutMeElement = document.getElementById("about_me")
+      const viewPortfolioElement = document.getElementById("view_portfolio")
+      const madeByElement = document.getElementById("made_by")
+      const aboutMeTextElement = document.getElementById("about_me_text")
+
+      if (aboutMeElement) aboutMeElement.textContent = translations.about_me
+
+      if (viewPortfolioElement)
+        viewPortfolioElement.textContent = translations.view_portfolio
+
+      if (madeByElement) madeByElement.textContent = translations.made_by
+
+      if (aboutMeTextElement)
+        aboutMeTextElement.textContent = translations.about_me_text
+    })
+    .catch((error) => console.error("Error loading translations:", error))
 }
